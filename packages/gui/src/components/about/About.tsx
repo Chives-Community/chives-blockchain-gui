89cbb6335b7f19782b9ffa2d81d53a107d49c734
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-04-27 16:32:02 -0500

    fix package name and about

--- packages/gui/src/components/about/About.tsx
+++ packages/gui/src/components/about/About.tsx
@@ -71,7 +71,7 @@ const Spacer = styled.div`
   margin-bottom: 1rem;
 `;
 
-const url = 'https://chia.net';
+const url = 'https://chivescoin.org';
 
 type Props = {
   version: string;
@@ -117,7 +117,7 @@ export default function About(props: Props) {
         </StyledLink>
         <StyledSubTitle>{description}</StyledSubTitle>
         <Spacer />
-        <div className="copyright">Copyright (c) {currentYear} Chia Network</div>
+        <div className="copyright">Copyright (c) {currentYear} Chives Network</div>
         <Spacer />
         <VersionsTable>
           {versions?.electron && (
